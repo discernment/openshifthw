714a7a93594f9d4587a10fe6df7b485e64b9061d
test <test@example.com>
2018-06-25 14:14:56 -0400

    test

--- src/main/java/com/redhat/qcon/insult/MainVerticle.java
+++ src/main/java/com/redhat/qcon/insult/MainVerticle.java
@@ -1,13 +1,40 @@
 package com.redhat.qcon.insult;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+//Be careful to use the appropriate import here:
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.core.AbstractVerticle;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) {
-
+        startFuture.complete(); 
+//        vertx.executeBlocking(future->
+//                {
+//                    
+//                    //future.complete(result);
+//                },
+//                result->-]{
+//                        
+//        }
+//        );
+        
+//        vertx.eventBus()
+//                .consumer("ping-timer")
+//                .toFlowable()
+//                .doOnEach(m->System.out.println(m.getValue().body()))
+//                .subscribe(m->m.reply(new JsonObject().put("action","PONG")));
+//        
+//        vertx.timerStream(300)
+//                .toObservable()
+//                .map(t->new JsonObject().put("action","PING"))
+//                .subscribe(ping ->vertx.eventBus()
+//                        .rxSend("ping-timer",ping)
+//                        .subscribe(m->System.out.println(m.body()))
+//                );
+//        
+//        
     }
 
 }
